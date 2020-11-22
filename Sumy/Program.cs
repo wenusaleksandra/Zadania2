94847f8e1aa3ff36f6051e55e009b4f93a7525fc
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-11-22 10:39:28 +0100

    Add project files.

---
+++ Sumy/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sumy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = int.Parse(Console.ReadLine());
+            var lists = new List<List<List<int>>>();
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                var listOfLists = Sumy(n);
+                lists.Add(listOfLists);
+            }
+            foreach (var item in lists)
+            {
+                foreach (var item2 in item)
+                {
+                    Console.WriteLine(string.Join(" ", item2));
+                }
+                Console.WriteLine();
+            }
+        }
+        private static List<List<int>> Sumy(int n)
+        {
+            var list = new List<List<int>>();
+            if (n==1)
+            {
+                list.Add(new List<int>() { 1 });
+                return list;
+            }
+            else
+            {
+                var poprzednialista = Sumy(n - 1);
+                for (int i = 0; i < poprzednialista.Count; i++)
+                {
+                    poprzednialista[i].Add(1);
+                }
+                if (n > 5)
+                {
+                    int start;
+                    if (n % 2 == 0)
+                    {
+                        start = n / 2;
+                    }
+                    else start = (n - 1) / 2;
+                    for (int x = start; x > 2; x--)
+                    {
+                        var lista2 = new List<int>();
+                        int roznica = n - x;
+                        lista2.Add(x);
+                        lista2.Add(roznica);
+                        poprzednialista.Add(lista2);
+                    }
+                }
+
+                List<List<int>> nowelisty = new List<List<int>>();
+                foreach (var item in poprzednialista)
+                {
+                    if (item.Count > 2)
+                    {
+                        if (item[^1] == item[^2] && item[^2] != item[^3])
+                        {
+                            var lista3 = new List<int>();
+                            lista3.AddRange(item);
+                            lista3.Add(item[^1] + item[^2]);
+                            lista3.RemoveAt(lista3.Count - 2);
+                            lista3.RemoveAt(lista3.Count - 2);
+
+                            nowelisty.Add(lista3);
+                        }
+                    }
+                }
+                foreach (var item in nowelisty)
+                {
+                    poprzednialista.Add(item);
+                }
+
+                poprzednialista.Add(new List<int> { n });
+
+                foreach (var item in poprzednialista)
+                {
+                    item.Sort();
+                }
+               
+                var rlist = poprzednialista.Select(lst => lst.OrderBy(i => i).ToList())
+                    .OrderBy(lst => lst[0]).ToList();
+                return rlist;
+            }
+            
+        }
+    }
+}
