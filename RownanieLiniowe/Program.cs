3ccfa8dee58b10f42ebabace74083b7448e2bf8f
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-12-15 22:23:48 +0100

    Rowanie Liniowe

--- RownanieLiniowe/Program.cs
+++ RownanieLiniowe/Program.cs
@@ -6,7 +6,22 @@ namespace RownanieLiniowe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string txt = Console.ReadLine();
+            decimal a = decimal.Parse(txt.Split(' ')[0]);
+            decimal b = decimal.Parse(txt.Split(' ')[1]);
+            decimal c = decimal.Parse(txt.Split(' ')[2]);
+            if (a == 0)
+            {
+                if (b == c)
+                {
+                    Console.WriteLine("NWR");
+                }
+                else Console.WriteLine("BR");
+                return;
+            }
+            decimal x = Math.Round((c - b) / a, 2);
+            Console.WriteLine(x.ToString("N2"));
+            Console.ReadLine();
         }
     }
 }
