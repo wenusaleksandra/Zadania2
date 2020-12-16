1ffbe85fbcc55a7991954c3d31eccf1c10f8d131
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-12-16 22:22:22 +0100

    Kalkulator, Transponowanie macierzy

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaWynikow = new List<int>();
+            string txt;
+            while ((txt = Console.ReadLine()) != null)
+            {
+                string znak = txt.Split(' ')[0];
+                int liczba1 = int.Parse(txt.Split(' ')[1]);
+                int liczba2 = int.Parse(txt.Split(' ')[2]);
+
+                if (znak == "+")
+                {
+                    listaWynikow.Add( Convert.ToInt32(liczba1 + liczba2));
+                }
+                else if (znak == "-")
+                {
+                    listaWynikow.Add(Convert.ToInt32(liczba1 - liczba2));
+                }
+                else if (znak == "*")
+                {
+                    listaWynikow.Add(Convert.ToInt32(liczba1 * liczba2));
+                }
+                else if (znak == "/")
+                {
+                    listaWynikow.Add(Convert.ToInt32(liczba1 / liczba2));
+                }
+                else if (znak == "%")
+                {
+                    listaWynikow.Add(Convert.ToInt32(liczba1 % liczba2));
+                }
+            }
+            foreach (var item in listaWynikow)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ TransponowanieMacierzy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TransponowanieMacierzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = Console.ReadLine();
+            int m = int.Parse(txt.Split(' ')[0]);
+            int n = int.Parse(txt.Split(' ')[1]);
+            var macierz = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                string wiersz = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    macierz[i,j] = int.Parse(wiersz.Split(' ')[j]);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(macierz[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
