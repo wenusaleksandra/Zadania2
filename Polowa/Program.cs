7071bc1bf4567cf6556801f3df6b38cff4b9d4f0
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2021-01-19 09:50:52 +0100

    add Polowa anad PredkoscSrednia

---
+++ Polowa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Polowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                string input = Console.ReadLine();
+                var dlugosc = input.Length;
+                Console.WriteLine(input.Substring(0, dlugosc / 2));
+            }
+        }
+    }
+}
---
+++ PredkoscSrednia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PredkoscSrednia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                string input = Console.ReadLine();
+                int v1 = int.Parse(input.Split(' ')[0]);
+                int v2 = int.Parse(input.Split(' ')[1]);
+                var predkoscSrednia = 2 * v1 * v2 / (v1 + v2);
+                Console.WriteLine(predkoscSrednia);
+            }
+        }
+    }
+}
