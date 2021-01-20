bef402dfb4f63454dd1cb25dbd625221a4486ef4
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2021-01-20 13:49:46 +0100

    SimpleNumbersConversions - adding conversion from decimal to another type (nie ma na spoj); add KtoraWieksza

---
+++ KtoraWieksza/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KtoraWieksza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int a = int.Parse(input.Split(' ')[0]);
+            int b = int.Parse(input.Split(' ')[1]);
+            if (a == b)
+            {
+                Console.WriteLine("0");
+            }
+            if (a < b)
+            {
+                Console.WriteLine("2");
+            }
+            if (a > b)
+            {
+                Console.WriteLine("1");
+            }
+        }
+    }
+}
--- SimpleNumbersConversion/Program.cs
+++ SimpleNumbersConversion/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Numerics;
 
 namespace SimpleNumbersConversion
 {
@@ -11,20 +10,121 @@ namespace SimpleNumbersConversion
 
             for (int i = 0; i < liczbaTestow; i++)
             {
-                string input = Console.ReadLine();
-                string n = input.Split(' ')[0];
-                double r = double.Parse(input.Split(' ')[1]);
-                int s = int.Parse(input.Split(' ')[2]);
+                var input = Console.ReadLine();
+                var n = input.Split(' ')[0];
+                var r = double.Parse(input.Split(' ')[1]);
+                var s = int.Parse(input.Split(' ')[2]);
 
                 var liczba = n.ToCharArray();
-                double liczbaDziesietnie = 0;
+                var liczbaDziesietnie = 0;
                 for (int j = 0; j < liczba.Length; j++)
                 {
-                    Convert.ToInt64(liczba[j], 16);
-                    liczbaDziesietnie += int.Parse(liczba[j].ToString()) * Math.Pow(r, liczba.Length - j - 1);
+                    var parsowanie = int.TryParse(liczba[j].ToString(), out int result);
+                    
+                    if (!parsowanie)
+                    {
+                        switch (liczba[j].ToString())
+                        {
+                            case "A":
+                                result = 10;
+                                break;
+                            case "B":
+                                result = 11;
+                                break;
+                            case "C":
+                                result = 12;
+                                break;
+                            case "D":
+                                result = 13;
+                                break;
+                            case "E":
+                                result = 14;
+                                break;
+                            case "F":
+                                result = 15;
+                                break;
+                            case "G":
+                                result = 16;
+                                break;
+                            case "H":
+                                result = 17;
+                                break;
+                            case "I":
+                                result = 18;
+                                break;
+                            case "J":
+                                result = 19;
+                                break;
+                            case "K":
+                                result = 20;
+                                break;
+                            case "L":
+                                result = 21;
+                                break;
+                            case "M":
+                                result = 22;
+                                break;
+                            case "N":
+                                result = 23;
+                                break;
+                            case "O":
+                                result = 24;
+                                break;
+                            case "P":
+                                result = 25;
+                                break;
+                            case "Q":
+                                result = 26;
+                                break;
+                            case "R":
+                                result = 27;
+                                break;
+                            case "S":
+                                result = 28;
+                                break;
+                            case "T":
+                                result = 29;
+                                break;
+                            case "U":
+                                result = 30;
+                                break;
+                            case "V":
+                                result = 31;
+                                break;
+                            case "W":
+                                result = 32;
+                                break;
+                            case "X":
+                                result = 33;
+                                break;
+                            case "Y":
+                                result = 34;
+                                break;
+                            case "Z":
+                                result = 35;
+                                break;
+                        }
+                    }
+                    
+                    liczbaDziesietnie += result * (int)Math.Pow(r, liczba.Length - j - 1);
                 }
-                Console.WriteLine(liczbaDziesietnie);
+
+                var odwrocona = "";
+                
+                while (liczbaDziesietnie != 0)
+                {
+                    var reszta = liczbaDziesietnie % s;
+                    odwrocona += reszta.ToString();
+                    liczbaDziesietnie /= s;
+                }
+                Console.WriteLine(Reverse(odwrocona));
             }
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
