8f64c1bba41831e1cd9b4401dbb5bf61ca6f8fa8
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2021-01-20 13:03:04 +0100

    add SiecCzystoSwiatlowodowa

---
+++ PreczZKomentarzami/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PreczZKomentarzami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wierzcholki = new List<string>();
+            var krawedzie = new List<List<string>>();
+            var listySasiedztwa = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == null || input == string.Empty)
+                {
+                    break;
+                }
+                
+                string znak = input.Split(' ')[0];
+                string wierzcholek1 = input.Split(' ')[1];
+                string wierzcholek2 = input.Split(' ')[2];
+
+                if (znak == "B")
+                {
+                    krawedzie.Add(new List<string> { wierzcholek1, wierzcholek2 });
+
+                    if (!listySasiedztwa.ContainsKey(wierzcholek1))
+                    {
+                        listySasiedztwa.Add(wierzcholek1, new List<string>());
+                    }
+                    if (!listySasiedztwa.ContainsKey(wierzcholek2))
+                    {
+                        listySasiedztwa.Add(wierzcholek2, new List<string>());
+                    }
+
+                    listySasiedztwa[wierzcholek1].Add(wierzcholek2);
+                    listySasiedztwa[wierzcholek2].Add(wierzcholek1);
+
+                    if (!wierzcholki.Contains(wierzcholek1))
+                    {
+                        wierzcholki.Add(wierzcholek1);
+                    }
+                    if (!wierzcholki.Contains(wierzcholek2))
+                    {
+                        wierzcholki.Add(wierzcholek2);
+                    }
+                }
+
+                if (znak == "T")
+                {
+                    if (!wierzcholki.Contains(wierzcholek1) || !wierzcholki.Contains(wierzcholek2))
+                    {
+                        Console.WriteLine("N");
+                        continue;
+                    }
+
+                    var wynik = BFS (wierzcholek1, wierzcholek2, wierzcholki, listySasiedztwa);
+                    if (wynik)
+                    {
+                        Console.WriteLine("T");
+                    }
+                    else Console.WriteLine("N");
+                }
+            }
+        }
+
+        private static bool BFS (string s, string k, List<string> wierzcholki, Dictionary<string, List<string>> listySasiedztwa)
+        {
+            var flagDict = new Dictionary<string, bool>();
+            
+            foreach (var v in wierzcholki)
+            {
+                flagDict[v] = false;
+            }
+
+            var q = new Queue<string>();
+            flagDict[s] = true;
+            q.Enqueue(s);
+            while (q.Count > 0)
+            {
+                var v = q.Dequeue();
+                if (v == k)
+                {
+                    return true;
+                }
+                foreach (var w in listySasiedztwa[v])
+                {
+                    if (flagDict[w] == false)
+                    {
+                        flagDict[w] = true;
+                        q.Enqueue(w);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ SimpleNumbersConversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace SimpleNumbersConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                string input = Console.ReadLine();
+                string n = input.Split(' ')[0];
+                double r = double.Parse(input.Split(' ')[1]);
+                int s = int.Parse(input.Split(' ')[2]);
+
+                var liczba = n.ToCharArray();
+                double liczbaDziesietnie = 0;
+                for (int j = 0; j < liczba.Length; j++)
+                {
+                    Convert.ToInt64(liczba[j], 16);
+                    liczbaDziesietnie += int.Parse(liczba[j].ToString()) * Math.Pow(r, liczba.Length - j - 1);
+                }
+                Console.WriteLine(liczbaDziesietnie);
+            }
+        }
+    }
+}
