e57c3f2abd4ee92f7cca15793b13f73e14240520
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2021-01-20 08:41:11 +0100

    Add Dodawanie

---
+++ CwanyLutek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace CwanyLutek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                string input = Console.ReadLine();
+                BigInteger a = BigInteger.Parse(input.Split(' ')[0]);
+                BigInteger b = BigInteger.Parse(input.Split(' ')[1]);
+
+                Console.WriteLine(a+b);
+               
+            }
+        }
+    }
+}
