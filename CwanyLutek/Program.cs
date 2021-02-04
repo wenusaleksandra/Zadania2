7d117f4af3584530f8a3e03e05733ec6cfdcc0ea
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2021-02-04 10:05:36 +0100

    add TagiHTML

--- CwanyLutek/Program.cs
+++ CwanyLutek/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Numerics;
 
-namespace CwanyLutek
+namespace Dodawanie
 {
     class Program
     {
---
+++ TagiHTML/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TagiHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+                string converted = "";
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == 60)
+                    {
+                        while (input[i] != 62)
+                        {
+                            if (input[i] >=97 && input[i] <= 122)
+                            {
+                                converted += input[i].ToString().ToUpper();
+                            }
+                            else converted += (char)input[i];
+                            i++;
+                        }
+                    }
+                    converted += (char) input[i];
+                }
+                Console.WriteLine(converted);
+            }
+        }
+    }
+}
